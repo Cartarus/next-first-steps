4ab9474ec813a5223a2f78e00b6586abfe1d44a4
Cristian Guerrero <cristian.guerrero@uptc.edu.co>
2024-03-17 15:09:33 -0500

    first commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+ title: 'Seo contact',
+ description: 'contacto  Cristian Guerrero',
+ keywords:['correo Cistian Guerrero', 'contacto']
+};
+
+export default function ContactPage() {
+    return (
+        <>
+            <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
